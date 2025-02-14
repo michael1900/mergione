393fd2294386b261626ff3a96b1b44680b7a36e4
emaschi123 <test-stremio123@proton.me>
2025-02-14 16:23:00 +0100

    Update def.py

--- def.py
+++ def.py
@@ -116,7 +116,7 @@ def save_m3u8(organized_channels):
         for service, categories in organized_channels.items():
             for category, channels in categories.items():
                 for name, url, base_url, user_agent in channels:
-                    f.write(f'#EXTINF:-1 tvg-id="" tvg-name="{name}" group-title="{category}" http-user-agent="{user_agent}/2.6" http-referrer="{base_url}",{name}\n')
+                    f.write(f'#EXTINF:-1 tvg-id="" tvg-name="{name}" tvg-logo="https://raw.githubusercontent.com/stremioita/eventi/refs/heads/main/vavoo.png" group-title="{category}" http-user-agent="{user_agent}/2.6" http-referrer="{base_url}",{name}\n')
                     f.write(f"#EXTVLCOPT:http-user-agent={user_agent}/2.6\n")
                     f.write(f"#EXTVLCOPT:http-referrer={base_url}/\n")
                     f.write(f'#EXTHTTP:{{"User-Agent":"{user_agent}/2.6","Referer":"{base_url}/"}}\n')
